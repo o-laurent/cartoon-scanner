82b631d5e70976ad2141b15d4609a6c7bfa1e5b8
Olivier Laurent <olivier.laurent@ensta-paris.fr>
2020-08-28 17:51:24 +0200

    formatting documents

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 #   améliorer l'interface graphique
 #   mettre l'interface graphique dans un autre fichier
 #   import tkinter as tk
-#   rotation de la photo suivant plusieurs angles 
-#   moyenne des traits du bord, min et max 
+#   rotation de la photo suivant plusieurs angles
+#   moyenne des traits du bord, min et max, noircir le trait à fond
 
 # load and show an image with Pillow
 from PIL import Image
-import numpy as np 
+import numpy as np
 from imageTools import sharpenImage, post_process
 from square_edge_detection import left_low_edge, left_top_edge, right_low_edge, right_top_edge, intersection_right
 # to load the list of files
@@ -17,58 +17,65 @@ from os.path import isfile, join
 #imgName = imgNameInput.get()
 seriesName = 'plaid'
 
+
 def isBlack(arr):
     var = int(arr[0])+int(arr[1])+int(arr[2])
-    return (var<320) and arr[0]<120 and arr[1]<120 and arr[2]<120
+    return (var < 320) and arr[0] < 120 and arr[1] < 120 and arr[2] < 120
+
 
 def isWhite(arr):
     var = int(arr[0])+int(arr[1])+int(arr[2])
-    return (var>500)
+    return (var > 500)
+
 
 def transX(sX, x):
     return x*sX
 
+
 def transY(sY, y):
     return y*sY
 
+
 def instaPrep(seriesName: str):
-    # Load list of files 
+    # Load list of files
     file_list = [f for f in listdir() if isfile(f)]
-    file_list = [f for f in file_list if f.split('.')[1].lower()=='jpg' and f.split('_')[0]==seriesName and f.split('_')[-1].split('.')[0]!='d' and (f.split('.')[0]+'_d.'+f.split('.')[1] not in file_list)]
+    file_list = [f for f in file_list if f.split('.')[1].lower() == 'jpg' and f.split('_')[0] == seriesName and f.split(
+        '_')[-1].split('.')[0] != 'd' and (f.split('.')[0]+'_d.'+f.split('.')[1] not in file_list)]
     file_list = list(map(lambda x: x.split('.')[0], file_list))
     file_list.sort()
     nb = len(file_list)
-    if nb==0:
+    if nb == 0:
         print('Aucune photo trouvée. Fin du processus.')
 
-    if nb==1:
+    if nb == 1:
         print('1 photo à traiter')
         digitalizeImage(seriesName+'_1.jpg')
 
-    if nb==2:
+    if nb == 2:
         print('2 photos à traiter')
         # digitalize separately
         for i in range(1, 3):
             print('Photo '+str(i)+' en traitement. Veuillez patienter.')
             digitalizeImage(seriesName+'_'+str(i)+'.jpg')
 
-    if nb==3:
+    if nb == 3:
         print('3 photos à traiter')
-        #déterminer laquelle est le rectangle
+        # déterminer laquelle est le rectangle
         # digitalize separately
         for i in range(1, 4):
             print('Photo '+str(i)+' en traitement. Veuillez patienter.')
             digitalizeImage(seriesName+'_'+str(i)+'.jpg')
 
-    if nb==4:
+    if nb == 4:
         print('4 photos à traiter')
         images = []
         # digitalize separately
         for i in range(1, 5):
             print('Photo '+str(i)+' en traitement. Veuillez patienter.')
             images.append(digitalizeImage(seriesName+'_'+str(i)+'.jpg', False))
-        # Merge 
+        # Merge
         merge4(seriesName, images[0], images[1], images[2], images[3])
+
 
 def digitalizeImage(imgName, ROTATION=True):
     if ROTATION:
@@ -84,9 +91,9 @@ def digitalizeImage(imgName, ROTATION=True):
     # matrixize and sharpen the image
     image = sharpenImage(imagePIL, imageGPIL, 0.75, 15)
 
-    # find the real dimensions 
-    # on commence par le haut au milieu, on cherche le premier pixel noir 
-    found = False 
+    # find the real dimensions
+    # on commence par le haut au milieu, on cherche le premier pixel noir
+    found = False
     mid = width//2
     maxI = 0
     i = 100
@@ -94,13 +101,14 @@ def digitalizeImage(imgName, ROTATION=True):
         if isBlack(image[i][mid]):
             j = -round(width/40)
             stop = False
-            while not stop and j<round(width/40):
-                foundJ = False 
+            while not stop and j < round(width/40):
+                foundJ = False
                 varI = -round(height/50)
                 while not foundJ and varI < round(height/50):
                     foundJ = isBlack(image[i+varI][mid+j])
                     varI += 1
-                stop = not foundJ #si on a pas trouvé de noir dans le rectangle, c'est pas un trait du carré
+                # si on a pas trouvé de noir dans le rectangle, c'est pas un trait du carré
+                stop = not foundJ
                 if foundJ:
                     maxI = i
                 j += 1
@@ -118,13 +126,14 @@ def digitalizeImage(imgName, ROTATION=True):
         if isBlack(image[i][mid]):
             j = -round(width/40)
             stop = False
-            while not stop and j<round(width/40):
-                foundJ = False 
+            while not stop and j < round(width/40):
+                foundJ = False
                 varI = -round(height/50)
                 while not foundJ and varI < round(height/50):
                     foundJ = isBlack(image[i+varI][mid+j])
                     varI += 1
-                stop = not foundJ #si on a pas trouvé de noir dans le rectangle, c'est pas un trait du carré
+                # si on a pas trouvé de noir dans le rectangle, c'est pas un trait du carré
+                stop = not foundJ
                 if foundJ:
                     maxI = i
                 j += 1
@@ -142,13 +151,14 @@ def digitalizeImage(imgName, ROTATION=True):
         if isBlack(image[mid][j]):
             i = -round(height/40)
             stop = False
-            while not stop and i<round(height/40):
-                foundI = False 
+            while not stop and i < round(height/40):
+                foundI = False
                 varJ = -round(width/50)
                 while not foundI and varJ < round(width/50):
                     foundI = isBlack(image[mid+i][j+varJ])
                     varJ += 1
-                stop = not foundI #si on a pas trouvé de noir dans le rectangle, c'est pas un trait du carré
+                # si on a pas trouvé de noir dans le rectangle, c'est pas un trait du carré
+                stop = not foundI
                 if stop:
                     print('missed')
                     print(mid+i)
@@ -168,13 +178,14 @@ def digitalizeImage(imgName, ROTATION=True):
         if isBlack(image[mid][j]):
             i = -round(height/40)
             stop = False
-            while not stop and i<round(height/40):
-                foundI = False 
+            while not stop and i < round(height/40):
+                foundI = False
                 varJ = -round(width/50)
                 while not foundI and varJ < round(width/50):
                     foundI = isBlack(image[mid+i][j+varJ])
                     varJ += 1
-                stop = not foundI #si on a pas trouvé de noir dans le rectangle, c'est pas un trait du carré
+                # si on a pas trouvé de noir dans le rectangle, c'est pas un trait du carré
+                stop = not foundI
                 if stop:
                     print('missed')
                     print(mid+i)
@@ -187,16 +198,20 @@ def digitalizeImage(imgName, ROTATION=True):
     rightThreshold = maxJ + 172
     print('right : '+str(maxJ))
 
-    # Find the edges 
+    # Find the edges
     leftTopEdge = left_top_edge(image, upperThreshold, width)
     rightTopEdge = right_top_edge(image, upperThreshold, width)
     rightLowerEdge = right_low_edge(image, lowerThreshold, width)
     leftLowerEdge = left_low_edge(image, lowerThreshold, width)
 
-    upper_length = ((leftTopEdge[0]-rightTopEdge[0])**2+(leftTopEdge[1]-rightTopEdge[1])**2)**(1/2)
-    lower_length = ((leftLowerEdge[0]-rightLowerEdge[0])**2+(leftLowerEdge[1]-rightLowerEdge[1])**2)**(1/2)
-    left_length = ((leftTopEdge[0]-leftLowerEdge[0])**2+(leftTopEdge[1]-leftLowerEdge[1])**2)**(1/2)
-    right_length = ((rightLowerEdge[0]-rightTopEdge[0])**2+(rightLowerEdge[1]-rightTopEdge[1])**2)**(1/2)
+    upper_length = ((leftTopEdge[0]-rightTopEdge[0])
+                    ** 2+(leftTopEdge[1]-rightTopEdge[1])**2)**(1/2)
+    lower_length = ((leftLowerEdge[0]-rightLowerEdge[0])
+                    ** 2+(leftLowerEdge[1]-rightLowerEdge[1])**2)**(1/2)
+    left_length = ((leftTopEdge[0]-leftLowerEdge[0])
+                   ** 2+(leftTopEdge[1]-leftLowerEdge[1])**2)**(1/2)
+    right_length = ((rightLowerEdge[0]-rightTopEdge[0])
+                    ** 2+(rightLowerEdge[1]-rightTopEdge[1])**2)**(1/2)
 
     print(lower_length)
     print('longueur côté haut : '+str(upper_length)+' pixels')
@@ -204,8 +219,8 @@ def digitalizeImage(imgName, ROTATION=True):
     print('longueur côté gauche : '+str(left_length)+' pixels')
     print('longueur côté droite : '+str(right_length)+' pixels')
 
-    # Rotation 
-    #On considère la droite passant par les deux coins droits et on cherche son intersection avec l'horizontale passant par le coin en bas à gauche
+    # Rotation
+    # On considère la droite passant par les deux coins droits et on cherche son intersection avec l'horizontale passant par le coin en bas à gauche
     if ROTATION:
         i_A = leftLowerEdge[0]
         i_C = rightTopEdge[0]
@@ -217,15 +232,16 @@ def digitalizeImage(imgName, ROTATION=True):
         is_D = -i_D + i_A
         jp_C = j_C - j_A
         jp_D = j_D - j_A
-        
+
         a = (is_C-is_D)/(jp_C-jp_D)
         b = -jp_D*a
 
         right_intersection = [i_A, j_A-b/a]
-        rightLowEdge_dist = ((right_intersection[0]-rightLowerEdge[0])**2 + (right_intersection[1]-rightLowerEdge[1])**2)**(1/2)
+        rightLowEdge_dist = ((right_intersection[0]-rightLowerEdge[0])**2 + (
+            right_intersection[1]-rightLowerEdge[1])**2)**(1/2)
 
         angleRLE = np.arctan(rightLowEdge_dist/lower_length)*180/np.pi
-        if i_A > i_D: #negative angle
+        if i_A > i_D:  # negative angle
             angleRLE *= -1
 
         print(angleRLE)
@@ -233,7 +249,8 @@ def digitalizeImage(imgName, ROTATION=True):
         angleRLE = 0
 
     # Crop and sharpen the image
-    box = (leftThreshold, upperThreshold, rightThreshold, lowerThreshold) #left upper right lower
+    box = (leftThreshold, upperThreshold, rightThreshold,
+           lowerThreshold)  # left upper right lower
     imagePIL = imagePIL.rotate(angleRLE).crop(box)
     imageGPIL = imagePIL.convert('L')
 
@@ -246,15 +263,15 @@ def digitalizeImage(imgName, ROTATION=True):
     sX = height/2800
     sY = width/2800
 
-    # Build the new image 
+    # Build the new image
 
     for x in range(2800):
         for y in range(2800):
-            color = image[int(transX(sX,x))][int(transY(sY, y))]
+            color = image[int(transX(sX, x))][int(transY(sY, y))]
             if not isWhite(color):
                 newImage[x][y] = color
-            else: 
-                newImage[x][y] = [255,255,255]
+            else:
+                newImage[x][y] = [255, 255, 255]
     newImage = post_process(newImage)
     numImagePIL = Image.fromarray(newImage.astype('uint8'), 'RGB')
 
@@ -264,19 +281,24 @@ def digitalizeImage(imgName, ROTATION=True):
     numImagePIL.save(imgName.split('.')[0]+'_d.jpg')
     return newImage
 
+
 def merge4(seriesName, image1, image2, image3, image4):
     print('Assemblage')
-    box1 = (95, 95, 1363, 1363) #include centering (172/2=86)
+    box1 = (95, 95, 1363, 1363)  # include centering (172/2=86)
     box2 = (1437, 95, 2705, 1363)
     box3 = (95, 1437, 1363, 2705)
     box4 = (1437, 1437, 2705, 2705)
-    image1PIL = Image.fromarray(image1.astype('uint8'), 'RGB').resize((1400, 1400)).crop((66,66,1400-66,1400-66))
-    image2PIL = Image.fromarray(image2.astype('uint8'), 'RGB').resize((1400, 1400)).crop((66,66,1400-66,1400-66))
-    image3PIL = Image.fromarray(image3.astype('uint8'), 'RGB').resize((1400, 1400)).crop((66,66,1400-66,1400-66))
-    image4PIL = Image.fromarray(image4.astype('uint8'), 'RGB').resize((1400, 1400)).crop((66,66,1400-66,1400-66))
+    image1PIL = Image.fromarray(image1.astype('uint8'), 'RGB').resize(
+        (1400, 1400)).crop((66, 66, 1400-66, 1400-66))
+    image2PIL = Image.fromarray(image2.astype('uint8'), 'RGB').resize(
+        (1400, 1400)).crop((66, 66, 1400-66, 1400-66))
+    image3PIL = Image.fromarray(image3.astype('uint8'), 'RGB').resize(
+        (1400, 1400)).crop((66, 66, 1400-66, 1400-66))
+    image4PIL = Image.fromarray(image4.astype('uint8'), 'RGB').resize(
+        (1400, 1400)).crop((66, 66, 1400-66, 1400-66))
     newImage = np.ones((2800, 2800, 3))*255
     newImagePIL = Image.fromarray(newImage.astype('uint8'), 'RGB')
-    
+
     signaturePIL = Image.open('./signature/signature.jpg').resize((75, 600))
     boxs = (2800-115-50-50, 2800-115-600-10, 2800-115-25, 2800-115-10)
 
--- square_edge_detection.py
+++ square_edge_detection.py
@@ -1,33 +1,39 @@
 # load and show an image with Pillow
 from PIL import Image
-import numpy as np 
+import numpy as np
 from imageTools import sharpenImage
 
 # to load the list of files
 from os import listdir
 from os.path import isfile, join
 
+
 def isBlack(arr):
     var = int(arr[0])+int(arr[1])+int(arr[2])
-    return (var<320) and arr[0]<120 and arr[1]<120 and arr[2]<120
+    return (var < 320) and arr[0] < 120 and arr[1] < 120 and arr[2] < 120
+
 
 def isWhite(arr):
     var = int(arr[0])+int(arr[1])+int(arr[2])
-    return (var>500)
+    return (var > 500)
+
 
 def transX(sX, x):
     return x*sX
 
+
 def transY(sY, y):
     return y*sY
 
+
 def isCorner(image, i, j):
     boolean = True
-    while boolean and j>172:
+    while boolean and j > 172:
         boolean = not isBlack(image[i][j])
-        j-=1
+        j -= 1
     #print([i, j])
     return boolean
+
 
 """imagePIL = Image.open(imgName)
 imageGPIL = imagePIL.convert('L')
@@ -145,26 +151,28 @@ while not found and j > leftThreshold + 100:
 rightThreshold = maxJ + 172
 print('right : '+str(maxJ))"""
 
-# Coin supérieur gauche 
+# Coin supérieur gauche
+
+
 def left_top_edge(image, upperThreshold, width):
     """ runs along the cartoon strokes to find the left top edge """
     i = upperThreshold + 172
     j = width//2
-    corner = False 
+    corner = False
     while not corner:
         while isBlack(image[i][j]):
             j -= 1
         up = 0
-        for varI in range(1,11):
+        for varI in range(1, 11):
             up += int(isBlack(image[i-varI][j]))
 
         down = 0
-        for varI in range(1,11):
+        for varI in range(1, 11):
             down += int(isBlack(image[i+varI][j]))
 
         left = 0
-        for varI in range(-5,6):
-            for varJ in range(1,6):
+        for varI in range(-5, 6):
+            for varJ in range(1, 6):
                 left += int(isBlack(image[i+varI][j-varJ]))
 
         if left < 10:
@@ -176,26 +184,28 @@ def left_top_edge(image, upperThreshold, width):
     print(i, j-1)
     return [i, j-1]
 
-# Coin supérieur droit 
+# Coin supérieur droit
+
+
 def right_top_edge(image, upperThreshold, width):
     """ runs along the cartoon strokes to find the right top edge """
     i = upperThreshold + 172
     j = width//2
-    corner = False 
+    corner = False
     while not corner:
         while isBlack(image[i][j]):
             j += 1
         up = 0
-        for varI in range(1,11):
+        for varI in range(1, 11):
             up += int(isBlack(image[i-varI][j]))
 
         down = 0
-        for varI in range(1,11):
+        for varI in range(1, 11):
             down += int(isBlack(image[i+varI][j]))
 
         right = 0
-        for varI in range(-5,6):
-            for varJ in range(1,6):
+        for varI in range(-5, 6):
+            for varJ in range(1, 6):
                 right += int(isBlack(image[i+varI][j+varJ]))
 
         if right < 10:
@@ -207,26 +217,28 @@ def right_top_edge(image, upperThreshold, width):
     print(i, j+1)
     return [i, j+1]
 
-# Coin inférieur gauche 
+# Coin inférieur gauche
+
+
 def left_low_edge(image, lowerThreshold, width):
     """ runs along the cartoon strokes to find the left low edge """
     i = lowerThreshold - 172
     j = width//2
-    corner = False 
+    corner = False
     while not corner:
         while isBlack(image[i][j]):
             j -= 1
         up = 0
-        for varI in range(1,11):
+        for varI in range(1, 11):
             up += int(isBlack(image[i-varI][j]))
 
         down = 0
-        for varI in range(1,11):
+        for varI in range(1, 11):
             down += int(isBlack(image[i+varI][j]))
 
         left = 0
-        for varI in range(-5,6):
-            for varJ in range(1,6):
+        for varI in range(-5, 6):
+            for varJ in range(1, 6):
                 left += int(isBlack(image[i+varI][j-varJ]))
 
         if left < 10:
@@ -238,26 +250,28 @@ def left_low_edge(image, lowerThreshold, width):
     print(i, j-1)
     return [i, j-1]
 
-# Coin inférieur droit 
+# Coin inférieur droit
+
+
 def right_low_edge(image, lowerThreshold, width):
     """ runs along the cartoon strokes to find the right low edge """
     i = lowerThreshold - 172
     j = width//2
-    corner = False 
+    corner = False
     while not corner:
         while isBlack(image[i][j]):
             j += 1
         up = 0
-        for varI in range(1,11):
+        for varI in range(1, 11):
             up += int(isBlack(image[i-varI][j]))
 
         down = 0
-        for varI in range(1,11):
+        for varI in range(1, 11):
             down += int(isBlack(image[i+varI][j]))
 
         right = 0
-        for varI in range(-5,6):
-            for varJ in range(1,6):
+        for varI in range(-5, 6):
+            for varJ in range(1, 6):
                 right += int(isBlack(image[i+varI][j+varJ]))
 
         if right < 10:
@@ -283,4 +297,4 @@ def intersection_right(image, width, leftLowEdge, lower_length):
             j -= 1
             stack.pop(0)
             stack.append(isBlack(image[i][j]))
-    return [i,j+7]
+    return [i, j+7]
